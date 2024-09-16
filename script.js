7ae680fadb83f66e6349017a1ac462ad8a178e15
Ostoco <135153527+Ostoco@users.noreply.github.com>
2024-09-16 08:08:47 -0400

    Clase Model
    Clase Model agregada al script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+class Model {
+    constructor() {
+        this.name = '';
+    }
 
+    setName(newName) {
+        this.name = newName;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
